961c1e098494be251c691d8b48acf8a960eabb2e
Pablo Covarrubias <pablo.covarrubias@babel.es>
2018-11-17 02:37:15 +0100

    update Contamination with methods

--- contracts/Contamination.sol
+++ contracts/Contamination.sol
@@ -5,6 +5,8 @@ import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract Contamination is Ownable{
    
+    uint totalEthereumForOwner;
+
     struct Company{    
         string companyName;
         bool active;
@@ -14,7 +16,7 @@ contract Contamination is Ownable{
         mapping(string => uint) penalty;
         mapping(string => uint) current;
         uint32 lastPollutionDate;
-        uint32 totalPollutionAggregated;
+        uint32 totalPollutionAlerts;
         address sensor;
     }
 
@@ -35,7 +37,7 @@ contract Contamination is Ownable{
         return true;
     }
 
-    function registerSensor(address _companyAddress, address _sensorAddress) external {
+    function registerSensor(address _companyAddress, address _sensorAddress) external onlyOwner{
         Company memory company = companies[companyToOwner[_companyAddress]];
         company.sensor = _sensorAddress;
         companies[companyToOwner[_companyAddress]] = company;
@@ -47,16 +49,40 @@ contract Contamination is Ownable{
         companies[companyToOwner[_companyAddress]] = company;
     }
 
-    function activateCompany(address _companyAddress,uint amount) external {
+    function activateCompany(address _companyAddress, uint amount) external onlyOwner{
         Company memory company = companies[companyToOwner[_companyAddress]];
         require(company.currentDeposit >= amount,"Company dont have enough funds");
         company.active = true;
         companies[companyToOwner[_companyAddress]] = company;
     }
+
+    function setPollutionThreshold(address _companyAddress, string _pollutionType, uint _warning, uint _penalty) external onlyOwner{
+        Company storage company = companies[companyToOwner[_companyAddress]];
+        company.warning[_pollutionType] = _warning;
+        company.penalty[_pollutionType] = _penalty;
+        companies[companyToOwner[_companyAddress]] = company;//Verificar si es necesario
+    }
     
     function getCompanyInfo(address _companyAddress) external view returns (string,bool,address){
         Company memory company = companies[companyToOwner[_companyAddress]];
         return (company.companyName, company.active, company.sensor);
+    }
+
+    function setCurrentPollution(address _companyAddress, string _pollutionType, uint _current) external {
+        Company storage company = companies[companyToOwner[_companyAddress]];
+        company.current[_pollutionType] = _current;
+        company.lastPollutionDate = uint32(now);
+        company.totalPollutionAlerts++;
+        if(company.current[_pollutionType] > company.penalty[_pollutionType]){
+            uint penalty = company.initialDeposit/100;// (company.current[_pollutionType] / company.penalty[_pollutionType])**company.totalPollutionAlerts;
+            company.currentDeposit -= penalty;
+            totalEthereumForOwner += penalty;
+        }
+
+        companies[companyToOwner[_companyAddress]] = company;
+
+
+        //enviar evento
     }
 
 }
